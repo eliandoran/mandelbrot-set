479827ec5185e6f84dbb8759f2316ee5a286390d
Elian Doran <elian.doran@ulbsibiu.ro>
2021-06-01 12:56:26 +0300

    Add first impl of Mandelbrot fractal

--- script.js
+++ script.js
@@ -1,7 +1,42 @@
+function inMandelbrotSet(x, y) {
+    let resultReal = x;
+    let resultImag = y;
+    for (let i=0; i<10; i++) {
+        const tempReal = (resultReal * resultReal - resultImag * resultImag + x);
+        const tempImag = (2 * resultReal * resultImag + y);
+        resultReal = tempReal;
+        resultImag = tempImag;        
+    }
+
+    if (resultReal * resultImag < 5) {
+        return true;
+    }
+
+    return false;
+}
+
 function draw(canvasEl) {
-    canvasEl.width=800;
-    canvasEl.height=600;
+    const width = 800;
+    const height = 600;
+
+    // Adjust canvas size.
+    canvasEl.width = width;
+    canvasEl.height = height;
+
     const ctx = canvasEl.getContext("2d");
+
+    const magnificationFactor = 600;
+    const panX = 0;
+    const panY = 0;
+    for (let x=0; x < width; x++) {
+        for (let y=0; y < height; y++) {
+            const absX = (x / magnificationFactor - panX);
+            const absY = (y / magnificationFactor - panY);
+            if (inMandelbrotSet(absX, absY)) {
+                ctx.fillRect(x, y, 1, 1);
+            }
+        }
+    }
 }
 
 const canvasEl = document.getElementById("drawing");
